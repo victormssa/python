8cfb8b3bbd88046aa5a8e4b5be8e0056e4a396db
Victor Manoel Soares Silva Alves <victormssalves@outlook.com>
2022-08-13 04:09:25 -0300

    Update printOperadores.py

--- 01 - Básico/printOperadores.py
+++ 01 - Básico/printOperadores.py
@@ -1,5 +1,5 @@
-num1 = int (input('Digite o primeiro número '))
-num2 = int (input('Digite o segundo número '))
+num1 = float (input('Digite o primeiro número: '))
+num2 = float (input('Digite o segundo número: '))
 print(num1+num2)
 print(num1-num2)
 print(num1*num2)

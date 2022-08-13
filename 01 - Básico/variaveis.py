b757846ae952449e3e532dfaf2d8a533cc913e64
Victor Manoel Soares Silva Alves <victormssalves@outlook.com>
2022-08-13 04:01:27 -0300

    Update variaveis.py

--- 01 - Básico/variaveis.py
+++ 01 - Básico/variaveis.py
@@ -1,7 +1,7 @@
 nome = 'Victor'
 idade = 21
 peso = 73.5
-print('Seu nome é', nome, 'sua idade é', idade, 'e seu peso é', peso,'kg')
+print('Seu nome é {}, sua idade é {}, e seu peso é {}kg'.format(nome, idade, peso))
 print('===========================================')
 nome = input('Qual é o seu nome? ')
 idade = input('Qual é a sua idade? ')

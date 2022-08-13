85d51567a4db3e0ed1f0d0c9b2053a42aa04fd25
Victor Manoel Soares Silva Alves <victormssalves@outlook.com>
2022-08-13 02:28:56 -0300

    commit

--- 0 - Básico/HelloWorld.py
+++ 0 - Básico/HelloWorld.py
@@ -1,2 +1,2 @@
-print('Hello, World!')
+print('Hello, World!', 'User 713')
 import this
---
+++ 0 - Básico/variaveis.py
@@ -0,0 +1,5 @@
+nome = 'Victor'
+idade = 21
+peso = 73.5
+
+print('Seu nome é', nome, ',sua idade é', idade, 'e seu peso é', peso,'kg')
